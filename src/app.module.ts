3aa0d2a84207e5a36882b9172b154e6602abde39
bala0708 <36028824+bala0708@users.noreply.github.com>
2024-12-04 01:00:31 +0530

    Update app.module.ts
    port listening

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,24 +15,41 @@ import { VideosModule } from './videos/videos.module';
 import { TVDevicesModule } from './tv-devices/tv-devices.module';
 import { CampaignsModule } from './campaigns/campaigns.module';
 
+// @Module({
+//   imports: [
+//     // Load configuration from .env
+//     ConfigModule.forRoot({
+//       isGlobal: true, // Make the configuration available globally
+//     }),
+
+//     // TypeOrmModule configuration
+//     TypeOrmModule.forRoot({
+//       type: 'mysql', // Database type
+//       host: process.env.DB_HOST || '127.0.0.1', // DB Host
+//       port: +process.env.DB_PORT || 3306, // DB Port
+//       username: process.env.DB_USERNAME || 'root', // DB Username
+//       password: process.env.DB_PASSWORD || '', // DB Password
+//       database: process.env.DB_NAME || 'defaultdb', // Database name
+//       entities: [User, Session, Video, TVDevice, Log, AdminAction, Campaigns],
+//       synchronize: false, // Auto sync schema (don't use in production)
+//       logging: true, // Enable logging
+//     }),
+
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
 @Module({
   imports: [
-    // Load configuration from .env
-    ConfigModule.forRoot({
-      isGlobal: true, // Make the configuration available globally
-    }),
-
-    // TypeOrmModule configuration
     TypeOrmModule.forRoot({
-      type: 'mysql', // Database type
-      host: process.env.DB_HOST || '127.0.0.1', // DB Host
-      port: +process.env.DB_PORT || 3306, // DB Port
-      username: process.env.DB_USERNAME || 'root', // DB Username
-      password: process.env.DB_PASSWORD || '', // DB Password
-      database: process.env.DB_NAME || 'defaultdb', // Database name
-      entities: [User, Session, Video, TVDevice, Log, AdminAction, Campaigns],
-      synchronize: false, // Auto sync schema (don't use in production)
-      logging: true, // Enable logging
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [/* Your entities here */],
+      synchronize: true,  // Disable this in production
     }),
 
     // Your modules
